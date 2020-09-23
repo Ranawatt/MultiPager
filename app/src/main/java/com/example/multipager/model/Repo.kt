980d4ebcef09b7c9490779761850fb3c2409cb30
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-09-23 22:49:48 +0530

    Added ktx plugin Libraries

--- app/src/main/java/com/example/multipager/model/Repo.kt
+++ app/src/main/java/com/example/multipager/model/Repo.kt
@@ -10,8 +10,9 @@ import com.google.gson.annotations.SerializedName
  * with the serialized name.
  * This class also defines the Room repos table, where the repo [id] is the primary key.
  */
+@Entity(tableName = "repos")
 data class Repo(
-    @field:SerializedName("id") val id: Long,
+    @PrimaryKey @field:SerializedName("id") val id: Long,
     @field:SerializedName("name") val name: String,
     @field:SerializedName("full_name") val fullName: String,
     @field:SerializedName("description") val description: String?,
